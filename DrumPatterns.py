b2214642f1b516dc75b7aae6f247e5ee1447e0cf
David Neiman <dneiman@andrew.cmu.edu>
2016-01-25 18:35:43 -0500

    Added comment for clarity

--- DrumPatterns.py
+++ DrumPatterns.py
@@ -8,39 +8,39 @@ def fuzzyContains(x, myList, res):
             return 1
     return 0
 
-#beat: beat in measure
+#count: count in measure
 #res: Amount of subdivision
-def getCountSyllable(beat, res):
+def getCountSyllable(count, res):
     if(res == 2):
-        if(beat == 1):
+        if(count == 2):
             return "+"
     if(res == 3):
-        if(beat == 1):
+        if(count == 1):
             return "trip"
-        if(beat == 2):
+        if(count == 3):
             return "let"
     if(res == 4):
-        if(beat == 1):
+        if(count == 2):
             return "e"
-        if(beat == 2):
+        if(count == 3):
             return "+"
-        if(beat == 3):
+        if(count == 4):
             return "a"
-    return beat
+    return count
 
 #Function to print MIDI values for percussion in a rhythm
-#Arguments: nbeats: number of beats per measure
-#bpm: Number of beats per minute
+#Arguments: ncounts: number of counts per measure
+#bpm: Number of counts per minute
 #nmeasures: Number of measures to be played
 #resolution: Amount of subdivision (2 for 8th notes, 4 for 16ths, etc.)
-#snarebeats: List of beats on which the snare should play
-#bassbeats: List of beats on which the bass drum should play
+#snarecounts: List of counts on which the snare should play
+#basscounts: List of counts on which the bass drum should play
 
-def playDrum(nbeats, bpm, nmeasures, resolution, snarebeats, bassbeats):
+def playDrum(ncounts, bpm, nmeasures, resolution, snarecounts, basscounts):
     for w in range(0, nmeasures):
         print("")
         print("Measure " + str(w+1))
-        for x in range(0, nbeats*resolution):
+        for x in range(0, ncounts*resolution):
             time.sleep(60.0 /bpm/resolution)
 
             if(x % resolution == 0):
@@ -48,10 +48,11 @@ def playDrum(nbeats, bpm, nmeasures, resolution, snarebeats, bassbeats):
             else:
                 print(getCountSyllable(x % resolution + 1, resolution))
             
-            if(fuzzyContains(1.0*x/resolution + 1, snarebeats, 0.01)):
+            if(fuzzyContains(1.0*x/resolution + 1, snarecounts, 0.01)):
                 print("Snare: MIDI 36")
-            if(fuzzyContains(1.0*x/resolution + 1, bassbeats, 0.01)):
+            if(fuzzyContains(1.0*x/resolution + 1, basscounts, 0.01)):
                 print("Bass drum: MIDI 38")
 
 print("Starting code")
-playDrum(4, 180, 3, 5, [1, 1.5, 2, 2.5, 3, 3.5, 4, 4.5], [4])
+#Arguments: Time signature, tempo, nmeasures, subdivision, snare, bass
+playDrum(3, 180, 3, 4, [1, 1.5, 2, 2.5, 3, 3.5, 4, 4.5], [4])
