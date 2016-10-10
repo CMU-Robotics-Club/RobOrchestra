73b5771c382c8059615446127e0b3f18ab737cd5
Satrat <sadkins@andrew.cmu.edu>
2016-10-10 17:25:35 -0400

    Fixed Snarebot code

--- Instruments/Snarebot/code/snarebot_servo_midi/snarebot_servo_midi.ino
+++ Instruments/Snarebot/code/snarebot_servo_midi/snarebot_servo_midi.ino
@@ -1,5 +1,11 @@
 #include <MIDI.h>
 #include <midi_Defs.h>
+#include <midi_Message.h>
+#include <midi_Namespace.h>
+#include <midi_Settings.h>
+
+#include <MIDI.h>
+#include <midi_Defs.h>
 #include <midi_Namespace.h>
 #include <midi_Settings.h>
 #include <Servo.h>
