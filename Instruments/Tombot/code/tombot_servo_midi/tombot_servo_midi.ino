f9057747691e168bf511bb36c8d44e04d56f48f9
Satrat <sadkins@andrew.cmu.edu>
2016-11-06 12:35:35 -0500

    Pushed new tomBot code, archived old version

--- Instruments/Tombot/code/tombot_servo_midi/tombot_servo_midi.ino
+++ Instruments/Tombot/code/tombot_servo_midi/tombot_servo_midi.ino
@@ -1,49 +1,60 @@
 #include <MIDI.h>
+#include <midi_Defs.h>
+#include <midi_Namespace.h>
+#include <midi_Settings.h>
 #include <Servo.h>
 
-Servo myservo;
+
+MIDI_CREATE_DEFAULT_INSTANCE();
+
+Servo servo1;
 Servo servo2;
+
+int which = 0;
+
+void handleNoteOn(byte channel, byte pitch, byte velocity)
+{
+  if(pitch == 37) {
+    
+     if(which == 1){
+      hit();
+      which = -1;
+    }else{
+      hit2();
+      which = 1;
+   }
+    
+  }
+}
 
 
 void setup()
 {
-  myservo.attach(10);
+  servo1.attach(10);
   servo2.attach(11);
+  MIDI.setHandleNoteOn(handleNoteOn);
+  MIDI.begin(MIDI_CHANNEL_OMNI);
+  MIDI.turnThruOn();
 }
-
-int which = 0;
 
 void loop()
 { 
-  
-  if(which == 1){
-    hit();
-    delay(500);
-    which = -1;
-  }else{
-    hit2();
-     delay(500);
-     which = 1;
-   }
-   
+  MIDI.read();
 }
 
 void hit() {
   
-  myservo.write(85);
-  delay(100);
-  myservo.write(103);
-  delay(100);
-
+  servo1.write(80);
+  delay(70);
+  servo1.write(98);
+  delay(10);
 }
-
 
 void hit2() {
   
-  servo2.write(85);
-  delay(100);
-  servo2.write(103);
-  delay(100);
+  servo2.write(80);
+  delay(70);
+  servo2.write(98);
+  delay(10);
 
 }
-
