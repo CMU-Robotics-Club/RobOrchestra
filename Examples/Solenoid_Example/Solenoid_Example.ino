ccb0356e7a70fb6abd01d038c21ede920aad3b4d
Sara Adkins <sadkins@andrew.cmu.edu>
2018-02-22 10:31:48 -0500

    Interactive demo now starts on C major

--- Examples/Solenoid_Example/Solenoid_Example.ino
+++ Examples/Solenoid_Example/Solenoid_Example.ino
@@ -1,24 +1,17 @@
-int solenoidPin = 3; //change to output pin connected on the Arduino
-int solenoidPin2 = 5;
-int solenoidPin3 = 7;
+int solenoidPin = 22; //change to output pin connected on the Arduino
+
 
 void setup() {
   pinMode(solenoidPin, OUTPUT); //sets pin to receive output
-  pinMode(solenoidPin2, OUTPUT);
   digitalWrite(solenoidPin, LOW);
-  digitalWrite(solenoidPin2, LOW);
-  digitalWrite(solenoidPin3, LOW);
  
 }
 
 void loop() {
   digitalWrite(solenoidPin, HIGH); //pushes solenoid out
-  digitalWrite(solenoidPin2, HIGH);
-  digitalWrite(solenoidPin3, HIGH);
+
   delay(500); //Wait 1000 milliseconds
   digitalWrite(solenoidPin, LOW); //releases push force
-  digitalWrite(solenoidPin2, LOW);
-  digitalWrite(solenoidPin3, LOW);
   delay(500); //Wait 1000 milliseconds
 
 }
