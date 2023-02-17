7927cf37d2c00c153a626ab4d118d40d66bd8908
David <dneiman@andrew.cmu.edu>
2023-02-16 20:46:55 -0500

    Code cleanup

--- Hardware+Low Level/Instruments/Ukulelebot/code/linearactuator_adaptivethreshold/linearactuator_adaptivethreshold.ino
+++ Hardware+Low Level/Instruments/Ukulelebot/code/linearactuator_adaptivethreshold/linearactuator_adaptivethreshold.ino
@@ -8,15 +8,8 @@
 const int feedback = A8; //potentiometer from actuator
 int position_LA = 0; //potentiometer from actuator
 int err;
-int errprev;
-int errdiff;
-int errint;
 
 int target = 500;
-double kp = 0.1;//0.1;
-double kd = 0;//0.1;
-double ki = 0.0;
-
 double dt = 10/1000.0; //seconds
 double tol = 100;
 
@@ -28,34 +21,17 @@ void setup()
   pinMode(AIN2, OUTPUT);
 
   Serial.begin(9600);
-
-  position_LA = analogRead(feedback);  // read the input on analog pin 0:
-  err = target - position_LA;
-  errprev = err;
 }
 
 void loop()
 {
 
 position_LA = analogRead(feedback);  // read the input on analog pin 0:
-//Serial.println(position_LA);
 err = target - position_LA;
-errdiff = err-errprev;
-errint += err*dt;
-double power = kp*err + kd*(errdiff)/dt + ki*errint;
 
-double cap = 0.01;
-if (abs(power) > cap){
-  power = power/abs(power)*cap;
-}
-
-fwd(0);
-power = 0;
-
-int mintol = 15;
-int maxtol = 100;
-double tolrat = 1.1; //tolrat > 1. How fast we change the adaptive threshold
-//Big tolrat tries to converge faster but might oscillate near mintol
+int mintol = 15; //Min size of dead zone (smaller means more precision but risks oscillation around target)
+int maxtol = 100; //Max size of dead zone (smaller lets us move at full speed for longer but risks oscillation around target)
+double tolrat = 1.1; //How fast we change the adaptive threshold. Keep this bigger than 1. Bigger means faster convergence but risks oscillation
 
 if (abs(err) < tol){
   brake();
@@ -80,30 +56,9 @@ else{
 
 Serial.print(err);
 Serial.print(" ");
-  Serial.println(tol);
+Serial.println(tol);
 
-/*
-if((position_LA >= prev) && position_LA < 250)
-  {
-    fwd(10);
-  }
-
-else if (( position_LA < prev) && position_LA > 750)
-{
-  rev(10);
-}*/
 delay(dt*1000); //milliseconds
- 
-  /*Serial.print("  Position LA  = ");
-  Serial.println(position_LA);*/
-
-  /*Serial.println(errdiff);
-  Serial.println(errint);*/
-
-  errprev = err;
-
-  //Serial.println(power);
-
 
 }//end void loop
 
@@ -123,32 +78,3 @@ void brake()
   digitalWrite(AIN2, HIGH);
   analogWrite(PWMA, 0);
 }
-
-
-/*int input = A0;
-int enA = A2;
-int in1 = 3;
-int in2 = 4;
-//int rotDirection = 0;
-//int pressed = false;
-void setup() {
-  pinMode(enA, OUTPUT);
-  pinMode(in1, OUTPUT);
-  pinMode(in2, OUTPUT);
-  pinMode(input, INPUT);
- 
-  // Set initial rotation direction
-  digitalWrite(in1, LOW);
-  digitalWrite(in2, HIGH);
-  //analogWrite(enA, 100);
-  delay(100);
-  Serial.begin(9600);
- 
-}
-void loop() {
-  analogWrite(enA, 100);
-  int positionMotor = analogRead(input);
-  Serial.println(positionMotor);
-  delay(100);
-}
-*/
