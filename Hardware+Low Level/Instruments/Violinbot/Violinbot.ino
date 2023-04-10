4e6a6ab80850db9f9315a5dbc726fbcc00c8f1e9
David <dneiman@andrew.cmu.edu>
2023-04-10 18:23:16 -0400

    Violinbot Code
    Not working yet, unclear if it's a code issue or a wiring issue

--- Hardware+Low Level/Instruments/Violinbot/Violinbot.ino
+++ Hardware+Low Level/Instruments/Violinbot/Violinbot.ino
@@ -1,5 +1,22 @@
+#include <MIDI.h>
+#include <midi_Defs.h>
+#include <midi_Namespace.h>
+#include <midi_Settings.h>
+
+#include "xylo.h"
+
+#define MYCHANNEL 1
+
+#define STARTNOTE 56
+#define ENDNOTE 83 //Equal to the highest note
+#define KEY_UP_TIME 40
+
+MIDI_CREATE_DEFAULT_INSTANCE();
+
+unsigned long startTime = 0;
+
 //PWM starts on 2
-#define PWMA 2
+#define PWMA 3
 #define PWMB 3 
 #define PWMC 4
 #define PWMD 5
@@ -44,8 +61,65 @@ double tolrat = 1.1; //How fast we change the adaptive threshold. Keep this bigg
 double dt = 10/1000.0; //seconds
 double tol[] = {100, 100, 100, 100};
 
+//Going to assume same positions on all strings, TODO fix later if needed
+int positions[] = {100, 200, 300, 400, 500, 600, 700}; //No open for now.
+
+void handleNoteOn(byte channel, byte pitch, byte velocity){
+  //This function queues up notes to be unplayed
+  if(channel != MYCHANNEL) return; //Only play channel1
+  if(velocity == 0){
+    handleNoteOff(channel, pitch, velocity);
+    return; //Ignore velocity 0
+  }
+  
+  int noteIndex = pitch;
+  //Rescale notes
+  while(noteIndex < STARTNOTE){
+    noteIndex += 12;
+  }
+  while(noteIndex > ENDNOTE){
+    noteIndex -= 12;
+  }
+
+  int pos = positions[(noteIndex-STARTNOTE)%7];
+  int string = (noteIndex-STARTNOTE)/7;
+  target[string] = pos;
+
+  //fingerNote(noteIndex);
+  turnOnString(string);
+
+  //digitalWrite(pinnumbers[noteIndex-STARTNOTE], HIGH);
+  //pintimes[noteIndex-STARTNOTE] = millis();
+}
+
+void handleNoteOff(byte channel, byte pitch, byte velocity){
+  //This function queues up notes to be unplayed
+  if(channel != MYCHANNEL) return; //Only play channel1
+  
+  int noteIndex = pitch;
+  //Rescale notes
+  while(noteIndex < STARTNOTE){
+    noteIndex += 12;
+  }
+  while(noteIndex > ENDNOTE){
+    noteIndex -= 12;
+  }
+
+  int pos = positions[(noteIndex-STARTNOTE)%7];
+  int string = (noteIndex-STARTNOTE)/7;
+  target[string] = pos;
+
+  //fingerNote(noteIndex);
+  turnOffString(string);
+
+  //digitalWrite(pinnumbers[noteIndex-STARTNOTE], HIGH);
+  //pintimes[noteIndex-STARTNOTE] = millis();
+}
+
 void setup()
 {
+  //xylo_init();
+  
   pinMode(feedback[0], INPUT);//feedback from actuator
   pinMode(PWM[0], OUTPUT);
   pinMode(IN1[0], OUTPUT);
@@ -67,11 +141,19 @@ void setup()
   pinMode(IN2[3], OUTPUT);
 
   Serial.begin(9600);
+  
+  MIDI.setHandleNoteOn(handleNoteOn);
+  MIDI.begin(MIDI_CHANNEL_OMNI);          // Launch MIDI and listen to channel 1
+  MIDI.turnThruOn();
+  startTime = millis();
 }
 
 void loop()
 {
-  for(int i = 0; i < 4; i++){
+  //Check for and process new MIDI messages, then if it's time to release notes, do that
+  MIDI.read();
+
+  for(int i = 0; i < 1; i++){
 
     int pos = analogRead(feedback[i]);
     int err = target[i] - pos;  // read the input
@@ -107,8 +189,23 @@ void loop()
     delay(dt*1000); //milliseconds
   }
 
-}//end void loop
+  /*for(int x = 0; x < nPins; x++){
+    if(millis() - pintimes[x] > KEY_UP_TIME){
+      int keyPin = pinnumbers[x]; // map the note to the pin
+      digitalWrite(keyPin, LOW);
+    }
+  }*/
+}
 
+void turnOnString(int string){
+  Serial.print("Turning on string ");
+  Serial.println(string);
+}
+
+void turnOffString(int string){
+  Serial.print("Turning off string ");
+  Serial.println(string);
+}
 
 void fwd(int motor)
 { digitalWrite(IN1[motor], HIGH);
---
+++ Hardware+Low Level/Instruments/Violinbot/xylo.h
@@ -0,0 +1,21 @@
+/**
+ * @file: xylo.h
+ * @brief: xylobot pins handling (hardware code)
+ *
+ * @author: Audrey Yeoh (ayeoh)
+ */
+
+#ifndef _XYLO_H_
+#define _XYLO_H_
+
+#ifdef __cplusplus
+extern "C"{
+#endif
+
+void xylo_init();   // initialize the pins used for xylobot
+
+#ifdef __cplusplus
+} // extern "C"
+#endif /* __cplusplus */
+
+#endif /* __XYLO_H_ */
