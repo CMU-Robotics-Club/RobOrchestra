3c9cf247017e3dd8f6f41de6c9bb7164b433c2ef
David <dneiman@andrew.cmu.edu>
2024-02-08 20:27:15 -0500

    Playing with transformer

--- Software/Test Code/pygameTest2.py
+++ Software/Test Code/pygameTest2.py
@@ -1,18 +1,26 @@
 import pygame 
-from pygame import midi
+from pygame import midi #pip3 install python-rtmidi
 import time
 import mido
-mido.set_backend('mido.backends.rtmidi')
-midi.init()
-assert(midi.get_init())
-mdata = mido.MidiFile("auldlangsyne.mid")
-otpts = mido.get_output_names()
-assert(len(otpts) > 0)
-outport = mido.open_output(otpts[0])
 
-lis = []
-for msg in mdata.tracks[1]:
-    if msg.type == 'note_on':
-        lis.append(msg.note)
+def main():
+    mido.set_backend('mido.backends.rtmidi')
+    midi.init()
+    assert(midi.get_init())
+    mdata = mido.MidiFile("auldlangsyne.mid")
 
-print(lis)
+    #Playback - either comment out block or open SimpleSynth
+    otpts = mido.get_output_names()
+    assert(len(otpts) > 0)
+    outport = mido.open_output(otpts[0])
+
+    lis = []
+    for msg in mdata.tracks[1]:
+        if msg.type == 'note_on':
+            lis.append(msg.note)
+
+    print(lis)
+    return lis
+
+if __name__ == "__main__":
+    main()
